802293fdc7746d0367ab96f02a54a8c883cc5271
Aragon <faragon12355@gmail.com>
2020-02-07 14:20:37 +0300

    OYE

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите первую цифру");
+    double x1, x2, y;
+    int v;
+    x1 = in.nextDouble();
+        System.out.println("Введите вторую цифру");
+    x2 = in.nextDouble();
+        System.out.println("Что необходимо сделать?\n1)Сложить\n2)Отнять\n");
+    v = in.nextInt();
+    if (v == 1) {y = x1+x2; System.out.println("Результат сложения = "+ y);}
+    if (v == 2) {y = x1-x2; System.out.println("Результат вычитания = "+ y);}
+    in.close();
+    }
+}
