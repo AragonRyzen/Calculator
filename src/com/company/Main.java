0f4bd4bb703d13aee6049ac8c907586730ebac3b
Vshivtsev Matvey <matba26@gmail.com>
2020-02-07 14:44:07 +0300

    Деление

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,10 +12,11 @@ public class Main {
     x1 = in.nextDouble();
         System.out.println("Введите вторую цифру");
     x2 = in.nextDouble();
-        System.out.println("Что необходимо сделать?\n1)Сложить\n2)Отнятьc ПИДОР\n");
+        System.out.println("Что необходимо сделать?\n1)Сложить\n2)Отнять\n4)Делить\n");
     v = in.nextInt();
     if (v == 1) {y = x1+x2; System.out.println("Результат сложения = "+ y);}
     if (v == 2) {y = x1-x2; System.out.println("Результат вычитания = "+ y);}
+    if (v == 4) {y = x1/x2; System.out.println("Результат деления = "+ y);}
     in.close();
     }
 }
