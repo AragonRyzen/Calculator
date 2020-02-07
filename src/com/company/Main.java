76b1ee93719309d586fc307ced309ccff9d01ac3
Aragon <faragon12355@gmail.com>
2020-02-07 14:43:58 +0300

    UMNOJENIE

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,10 +12,11 @@ public class Main {
     x1 = in.nextDouble();
         System.out.println("Введите вторую цифру");
     x2 = in.nextDouble();
-        System.out.println("Что необходимо сделать?\n1)Сложить\n2)Отнятьc ПИДОР\n");
+        System.out.println("Что необходимо сделать?\n1)Сложить\n2)Отнять\n3)Умножить");
     v = in.nextInt();
     if (v == 1) {y = x1+x2; System.out.println("Результат сложения = "+ y);}
     if (v == 2) {y = x1-x2; System.out.println("Результат вычитания = "+ y);}
+    if (v == 3) {y = x1*x2; System.out.println("Результат умножения = "+ y);}
     in.close();
     }
 }
